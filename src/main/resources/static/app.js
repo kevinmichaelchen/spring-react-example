6c2562ca67a501e59931e91040f48b2b9bd56f5d
Kevin Chen <kevinchen@Kevins-iMac.local>
2016-02-23 23:36:44 -0500

    Use ES6 imports for React component per file

--- src/main/resources/static/app.js
+++ src/main/resources/static/app.js
@@ -1,11 +1,9 @@
 'use strict';
 
-// tag::vars[]
 const React = require('react');
 const client = require('./client');
-// end::vars[]
+import EmployeeList from './employeelist.js';
 
-// tag::app[]
 class App extends React.Component {
 
 	constructor(props) {
@@ -25,46 +23,8 @@ class App extends React.Component {
 		)
 	}
 }
-// end::app[]
 
-// tag::employee-list[]
-class EmployeeList extends React.Component{
-	render() {
-		var employees = this.props.employees.map(employee =>
-			<Employee key={employee._links.self.href} employee={employee}/>
-		);
-		return (
-			<table>
-				<tr>
-					<th>First Name</th>
-					<th>Last Name</th>
-					<th>Description</th>
-				</tr>
-				{employees}
-			</table>
-		)
-	}
-}
-// end::employee-list[]
-
-// tag::employee[]
-class Employee extends React.Component{
-	render() {
-		return (
-			<tr>
-				<td>{this.props.employee.firstName}</td>
-				<td>{this.props.employee.lastName}</td>
-				<td>{this.props.employee.description}</td>
-			</tr>
-		)
-	}
-}
-// end::employee[]
-
-// tag::render[]
 React.render(
 	<App />,
 	document.getElementById('react')
 )
-// end::render[]
-
---
+++ src/main/resources/static/employeelist.js
@@ -0,0 +1,24 @@
+'use strict';
+
+const React = require('react');
+import Employee from './employee.js';
+
+class EmployeeList extends React.Component{
+    render() {
+        var employees = this.props.employees.map(employee =>
+            <Employee key={employee._links.self.href} employee={employee}/>
+        );
+        return (
+            <table>
+                <tr>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Description</th>
+                </tr>
+                {employees}
+            </table>
+        )
+    }
+}
+
+export default EmployeeList;
