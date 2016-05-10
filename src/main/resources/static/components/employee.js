64334d3f1966ff9471d9a2259c1807d65a1f5c95
Kevin Chen <kevinmichaelchen@gmail.com>
2016-05-10 00:09:45 -0400

    use 2 spaces for indent

--- src/main/resources/static/components/employee.js
+++ src/main/resources/static/components/employee.js
@@ -2,16 +2,16 @@
 
 const React = require('react');
 
-class Employee extends React.Component{
-    render() {
-        return (
-            <tr>
-                <td>{this.props.employee.firstName}</td>
-                <td>{this.props.employee.lastName}</td>
-                <td>{this.props.employee.description}</td>
-            </tr>
-        )
-    }
+class Employee extends React.Component {
+  render() {
+    return (
+      <tr>
+        <td>{this.props.employee.firstName}</td>
+        <td>{this.props.employee.lastName}</td>
+        <td>{this.props.employee.description}</td>
+      </tr>
+    )
+  }
 }
 
 export default Employee;
--- src/main/resources/static/containers/App.js
+++ src/main/resources/static/containers/App.js
@@ -2,31 +2,31 @@
 
 const client = require('../client');
 
-import React, { Component } from 'react'
-import EmployeeList from './../components/employeelist.js';
+import React, {Component} from "react";
+import EmployeeList from "./../components/employeelist.js";
 
 class App extends Component {
 
-	constructor(props) {
-		super(props);
-		this.state = {employees: []};
-	}
+  constructor(props) {
+    super(props);
+    this.state = {employees: []};
+  }
 
-	componentDidMount() {
-		console.log('mounted')
-		client({method: 'GET', path: '/api/employees'}).done(response => {
-			this.setState({employees: response.entity._embedded.employees});
-		});
-	}
+  componentDidMount() {
+    console.log('mounted')
+    client({method: 'GET', path: '/api/employees'}).done(response => {
+      this.setState({employees: response.entity._embedded.employees});
+    });
+  }
 
-	render() {
-		return (
-			<EmployeeList employees={this.state.employees}/>
-		)
-	}
+  render() {
+    return (
+      <EmployeeList employees={this.state.employees}/>
+    )
+  }
 }
 
 React.render(
-	<App />,
-	document.getElementById('react')
+  <App />,
+  document.getElementById('react')
 )
