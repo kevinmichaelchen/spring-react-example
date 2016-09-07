29e594ddd866b11399762b9e84e4e2c8a8f1ca3f
Kevin Chen <kevinmichaelchen@gmail.com>
2016-09-07 14:13:15 -0400

    use different beanstalk gradle plugin

--- src/main/java/com/kitchen/skizule/Application.java
+++ src/main/java/com/kitchen/skizule/Application.java
@@ -2,9 +2,17 @@ package com.kitchen.skizule;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @SpringBootApplication
 public class Application {
+	@RequestMapping(value = "/test", method = RequestMethod.GET)
+	public ResponseEntity index() {
+		return ResponseEntity.ok( "Test" );
+	}
+
 	public static void main(String[] args) {
 		SpringApplication.run(Application.class, args);
 	}
